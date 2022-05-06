61abd8814fddbb9d5382bdafcd5d9d366e8f219b
卫佳琪 <84362113@qq.com>
2022-05-06 11:08:53 +0800

    项目创建

---
+++ app/src/main/java/com/drifting/bureau/MainActivity.java
@@ -0,0 +1,20 @@
+package com.drifting.bureau;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+/**
+ * @Description: 
+ * @Author     : WeiJiaQI
+ * @Time       : 2022/5/6 10:39
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
